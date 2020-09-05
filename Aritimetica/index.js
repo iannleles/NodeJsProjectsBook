2050e14079c9b54b7d2eab19486559fca03acb2d
Iann Leles <iannleles@outlook.com>
2020-09-05 01:24:38 -0300

    Aritimetica

---
+++ Aritimetica/index.js
@@ -0,0 +1,29 @@
+//Declaracao e inicializacao de duas variaveis, troque os valores se quiser
+
+let a = 5
+let b = 2
+
+// varios exemplos de operacoes sobre variaveis
+
+console.log('a = ' + a)
+
+console.log('b = ' + b)
+
+console.log('a + b = ' + (a + b))
+
+console.log('a - b =' + (a - b))
+
+console.log('a * b =' + (a * b))
+
+console.log('a / b  =' + (a / b))
+
+console.log('a % b = ' + (a % b))
+
+console.log('a ++ = ' + (a++))
+
+console.log('--b =' + (--b))
+
+console.log('a = ' + a)
+
+console.log('b = ' + b)
+
